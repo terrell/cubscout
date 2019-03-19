1dad8e101126dfacb06dd0b6a733ca88583aaedf
Joren De Groof <git@jorendegroof.be>
2019-03-19 15:13:27 +0100

    Empty Gem structure

---
+++ lib/cubscout.rb
@@ -0,0 +1,6 @@
+require "cubscout/version"
+
+module Cubscout
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/cubscout_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe Cubscout do
+  it "has a version number" do
+    expect(Cubscout::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
