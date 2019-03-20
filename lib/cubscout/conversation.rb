77a912d06f408fe11e1e8d5ba3ee479c36a50898
Joren De Groof <git@jorendegroof.be>
2019-03-20 11:06:38 +0100

    Adds ability to create notes on a conversation
    New endpoints:
    * Create note
    * Find a single conversation
    * Find the threads of a single conversation

--- lib/cubscout/conversation.rb
+++ lib/cubscout/conversation.rb
@@ -1,5 +1,20 @@
 module Cubscout
   class Conversation < List
     path "conversations"
+
+    class << self
+      def find(id)
+        Cubscout.connection.get("#{read_path}/#{id}")
+      end
+
+      def threads(id)
+        Cubscout.connection.get("#{read_path}/#{id}/threads").body.dig("_embedded", "threads")
+      end
+
+      def create_note(id, attributes)
+        raise "Missing attribute `text` while creating new note" unless attributes.has_key?(:text)
+        Cubscout.connection.post("#{read_path}/#{id}/notes", attributes.to_json)
+      end
+    end
   end
 end
