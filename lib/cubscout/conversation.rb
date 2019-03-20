fd6c761220d18278f52c6ff7b946c36cb29dfa19
Fabien Loup <info@norydev.com>
2019-03-20 12:10:09 +0100

    Conversation: get threads of an instance, create note for an instance

--- lib/cubscout/conversation.rb
+++ lib/cubscout/conversation.rb
@@ -4,13 +4,21 @@ module Cubscout
 
     class << self
       def threads(id)
-        Cubscout.connection.get("#{read_path}/#{id}/threads").body.dig("_embedded", "threads")
+        Cubscout.connection.get("#{read_path}/#{id}/threads").body.dig("_embedded", "threads").map { |item| Object.new(item) }
       end
 
       def create_note(id, attributes)
         raise "Missing attribute `text` while creating new note" unless attributes.has_key?(:text)
         Cubscout.connection.post("#{read_path}/#{id}/notes", attributes.to_json)
       end
+    end
+
+    def threads
+      Conversation.threads(self.id)
+    end
+
+    def create_note(attributes)
+      Conversation.create_note(self.id, attributes)
     end
   end
 end
