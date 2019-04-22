b9b2386928e7bc3d9df77c7846fc8c3f052e4e58
Fabien Loup <info@norydev.com>
2019-04-22 10:16:46 +0100

    native error for missing attribute :text

--- lib/cubscout/conversation.rb
+++ lib/cubscout/conversation.rb
@@ -7,9 +7,8 @@ module Cubscout
         Cubscout.connection.get("#{read_path}/#{id}/threads").body.dig("_embedded", "threads").map { |item| Object.new(item) }
       end
 
-      def create_note(id, attributes)
-        raise "Missing attribute `text` while creating new note" unless attributes.has_key?(:text)
-        Cubscout.connection.post("#{read_path}/#{id}/notes", attributes.to_json).body
+      def create_note(id, text:, **attributes)
+        Cubscout.connection.post("#{read_path}/#{id}/notes", attributes.merge(text: text).to_json).body
       end
     end
 
